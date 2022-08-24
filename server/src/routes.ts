e0aff0c476a0313cf5792fb78e3c18d4f3fe1505
GabrSobral <Gabriel.Sobral1367@gmail.com>
2022-08-24 14:52:12 +0000

    feat: adding routes

---
+++ server/src/routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const router = Router();
+
+// User router
+router.post("/user/register", () => {});
+router.post("/user/login", () => {});
+
+export { router };
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import { router } from './routes';
 
 const app = express();
 
 const PORT = process.env.PORT || 3333;
 
 app.use(express.json());
+app.use(router);
 
 app.listen(PORT, () => console.log(`✔✨ Server started at: http://localhost:${PORT}`));
