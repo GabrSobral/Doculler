9b77d604ad53452e4d3eb59da1942b485af7259f
Sobral <Asgoth55@gmail.com>
2022-08-27 00:17:08 -0300

    feat: adding team member service

---
+++ server/src/application/services/teamMemberServices/create-team-member-service.ts
@@ -0,0 +1,46 @@
+import { TeamRepository } from "../../../../src/application/repositories/team-repository";
+import { UserRepository } from "../../../../src/application/repositories/user-repository";
+import { TeamMember } from "../../../../src/domain/entities/TeamMember";
+import { TeamMemberRepository } from "../../../../src/application/repositories/team-member-repository";
+
+interface CreateTeamMemberRequest {
+  team_id: string;
+  user_id: string;
+}
+
+export class CreateTeamMember {
+  constructor(
+    private teamMemberRepository: TeamMemberRepository,
+    private teamRepository: TeamRepository,
+    private userRepository: UserRepository,
+  ) {}
+
+  async execute(request: CreateTeamMemberRequest): Promise<TeamMember> {
+    const { team_id, user_id } = request;
+
+    if(!team_id)
+      throw new Error("No team_id provided.");
+
+    if(!user_id)
+      throw new Error("No user_id provided.");
+
+    const team = await this.teamRepository.findById(team_id);
+
+    if(!team)
+      throw new Error("No team was found.");
+
+    const user = await this.userRepository.findById(user_id);
+
+    if(!user)
+      throw new Error("No user was found.");
+
+    const newTeamMember = TeamMember.create({
+      team_id,
+      user_id
+    });
+
+    await this.teamMemberRepository.add(newTeamMember);
+
+    return newTeamMember;
+  }
+}
---
+++ server/src/application/services/teamMemberServices/create-team-member.spec.ts
@@ -0,0 +1,135 @@
+import { Team } from '../../../../src/domain/entities/Team';
+import { User } from '../../../../src/domain/entities/User';
+import { InMemoryTeamRepository } from '../../../../tests/repositories/in-memory-team-repository';
+import { InMemoryTeamMemberRepository } from '../../../../tests/repositories/in-memory-team-member-repository';
+import { CreateTeamMember } from './create-team-member-service';
+import { InMemoryUserRepository } from '../../../../tests/repositories/in-memory-user-repository';
+
+describe("create-team-member", () => {
+  it("should be able to create a new Team Member", async () => {
+    const inMemoryTeamMemberRepository = new InMemoryTeamMemberRepository();
+    const inMemoryTeamRepository = new InMemoryTeamRepository();
+    const inMemoryUserRepository = new InMemoryUserRepository();
+    
+    const user = User.create({
+      name: "Test",
+      email: "Test@test.com",
+      password: "test123",
+    });
+
+    const team = Team.create({
+      name: "Team Test",
+      description: "Lorem Ipsum Dolor Sit Amet"
+    });
+
+    inMemoryTeamRepository.items.push(team);
+    inMemoryUserRepository.items.push(user);
+
+    const createTeamMember = new CreateTeamMember(
+      inMemoryTeamMemberRepository,
+      inMemoryTeamRepository,
+      inMemoryUserRepository
+    );
+
+    const newTeamMember = await createTeamMember.execute({
+      user_id: user.id,
+      team_id: team.id,
+    });
+
+    expect(newTeamMember).toBeTruthy();
+  });
+
+  it("should not be able to create a new Team Member without team_id", async () => {
+    const inMemoryTeamMemberRepository = new InMemoryTeamMemberRepository();
+    const inMemoryTeamRepository = new InMemoryTeamRepository();
+    const inMemoryUserRepository = new InMemoryUserRepository();
+    
+    const createTeamMember = new CreateTeamMember(
+      inMemoryTeamMemberRepository,
+      inMemoryTeamRepository,
+      inMemoryUserRepository
+    );
+
+    const user = User.create({
+      name: "Test",
+      email: "Test@test.com",
+      password: "test123",
+    });
+
+    await expect(createTeamMember.execute({
+      user_id: user.id,
+      team_id: "",
+    })).rejects.toThrow();
+  });
+
+  it("should not be able to create a new Team Member without user_id", async () => {
+    const inMemoryTeamMemberRepository = new InMemoryTeamMemberRepository();
+    const inMemoryTeamRepository = new InMemoryTeamRepository();
+    const inMemoryUserRepository = new InMemoryUserRepository();
+    
+    const createTeamMember = new CreateTeamMember(
+      inMemoryTeamMemberRepository,
+      inMemoryTeamRepository,
+      inMemoryUserRepository
+    );
+
+    const team = Team.create({
+      name: "Team Test",
+      description: "Lorem Ipsum Dolor Sit Amet"
+    });
+
+    await expect(createTeamMember.execute({
+      user_id: "",
+      team_id: team.id,
+    })).rejects.toThrow();
+  });
+
+  it("should not be able to create a new Team Member with an invalid team_id", async () => {
+    const inMemoryTeamMemberRepository = new InMemoryTeamMemberRepository();
+    const inMemoryTeamRepository = new InMemoryTeamRepository();
+    const inMemoryUserRepository = new InMemoryUserRepository();
+
+    const user = User.create({
+      name: "Test",
+      email: "Test@test.com",
+      password: "test123",
+    });
+
+    inMemoryUserRepository.items.push(user);
+
+    const createTeamMember = new CreateTeamMember(
+      inMemoryTeamMemberRepository,
+      inMemoryTeamRepository,
+      inMemoryUserRepository
+    );
+
+    await expect(createTeamMember.execute({
+      user_id: user.id,
+      team_id: "testId",
+    })).rejects.toThrow();
+  });
+
+  it("should not be able to create a new Team Member with an invalid user_id", async () => {
+    const inMemoryTeamMemberRepository = new InMemoryTeamMemberRepository();
+    const inMemoryTeamRepository = new InMemoryTeamRepository();
+    const inMemoryUserRepository = new InMemoryUserRepository();
+    
+    const team = Team.create({
+      name: "Team Test",
+      description: "Lorem Ipsum Dolor Sit Amet"
+    });
+
+    inMemoryTeamRepository.items.push(team);
+
+    const createTeamMember = new CreateTeamMember(
+      inMemoryTeamMemberRepository,
+      inMemoryTeamRepository,
+      inMemoryUserRepository
+    );
+
+    await expect(createTeamMember.execute({
+      user_id: "testId",
+      team_id: team.id,
+    })).rejects.toThrow();
+  });
+})
