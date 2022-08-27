fd93595cc6e7fd605da19356291e7dfbbd65800e
Sobral <Asgoth55@gmail.com>
2022-08-27 00:15:49 -0300

    delete: changing path of team-service

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,4 +9,7 @@ const PORT = process.env.PORT || 3333;
 app.use(express.json());
 app.use(router);
 
-app.listen(PORT, () => console.log(`✔✨ Server started at: http://localhost:${PORT}`));
+app.listen(
+  PORT, 
+  () => console.log(`✔✨ Server started at: http://localhost:${PORT}`)
+);
